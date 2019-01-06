40c8582be0ed76ffce3a8c1d81d1357ee95c4c5e
Andrew Snek <ak.tually.yes@gmail.com>
2019-01-06 14:18:14 +0200

    add basic Vuetify template

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,115 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="britecore">
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      clipped
+      disable-resize-watcher
+      class="grey lighten-4"
+      app
+    >
+      <v-list
+        dense
+        class="grey lighten-4"
+      >
+        <template v-for="(item, i) in items">
+          <v-list-tile
+            :key="i"
+            :to="item.link"
+          >
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar color="amber" app absolute clipped-left>
+      <v-toolbar-side-icon @click="isAuthenticated ? drawer = !drawer : $router.push('/login')"></v-toolbar-side-icon>
+      <router-link to="/">
+        <span class="title black--text mx-3">Britecore&nbsp;<span class="font-weight-light">Insurance</span></span>
+      </router-link>
+
+      <span class="custom-loader" v-show="loading">
+        <v-icon light>cached</v-icon>
+      </span>
+      <v-spacer></v-spacer>
+
+      <router-link to="/login" v-if="!isAuthenticated">
+        <v-btn color="amber">
+          <v-icon>account_box</v-icon>
+          <span class="pl-1">LOGIN</span>
+        </v-btn>
+      </router-link>
+      <router-link to="/logout" v-else>
+        <v-btn color="amber">
+          <v-icon>account_box</v-icon>
+          <span class="pl-1">LOGOUT</span>
+        </v-btn>
+      </router-link>
+    </v-toolbar>
+
+    <v-container fluid fill-height class="grey lighten-4">
+          <transition name="slide-y-transition" mode="out-in">
+            <router-view  class="mt-5"/>
+          </transition>
+    </v-container>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    items: [
+      { icon: 'subject', text: 'Field Types', link: '/fieldtypes' },
+      { icon: 'tab_unselected', text: 'Abstract Risks', link: '/abstractrisks' },
+      { icon: 'tab', text: 'Risks', link: '/risks' }
+    ]
+  }),
+  computed: {
+    isAuthenticated () {
+      return false // TODO
     }
   }
 }
+</script>
+
+<style lang="scss">
+  #britecore main .container {
+    height: 660px;
+  }
+
+  .navigation-drawer__border {
+    display: none;
+  }
+
+  .text {
+    font-weight: 400;
+  }
+
+  a {
+    text-decoration: none;
+    color: black !important;
+  }
+
+  .custom-loader {
+    animation: loader 1s infinite;
+    display: flex;
+  }
+
+  @keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
 </style>
---
+++ frontend/src/components/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container>
+        <v-layout row>
+            <v-flex xs12 sm6 offset-sm3 class="text-xs-center">
+                <v-card>
+                    <v-card-text>
+                        <div class="display-3 font-weight-black">
+                            Welcome
+                        </div>
+                        <div class="headline">
+                            To proceed with risk management:
+                        </div>
+                        <ol class="body-2 text-xs-left">
+                            <li>Login</li>
+                            <li>Select appropriate section from the "hamburger" menu</li>
+                        </ol>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
